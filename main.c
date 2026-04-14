41f9489b8013d043d0499212b9bdd2323c4deaa7
leoddiez <mxm220208@utdallas.edu>
2026-04-14 12:45:17 -0500

    Create main.c with basic setup

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define int MAX_CUST = 50;
+#define int MAX_TELL = 3;
+
+int main() {
+  srand(time(NULL));
+
+  return 0;
+}
