35bafb3e53fcd03e4dbd7c64c3db8033039b67b0
leoddiez <mxm220208@utdallas.edu>
2026-04-16 18:34:28 +0000

    Created the seamaphores and pthreads in the main()

--- main.c
+++ main.c
@@ -11,5 +11,19 @@
 int main() {
   srand(time(NULL));
 
+  pthread_t teller[MAX_TELL];
+  pthread_t customer[MAX_CUST];
+  pthread_mutex_init(&queue_lock, NULL);
+
+  //Semowh-
+  // Im creating these going in order of the general description i wrote
+  // Minus the other pthread, i moved it with the others to make it look nicer lol
+  sem_init(&b_Open, 0, 0);
+  sem_init(&doors, 0, 2);
+  sem_init(&c_waiting, 0, 0);
+  sem_init(&teller_ready, 0, 0);
+  sem_init(&safe, 0, 2);
+  sem_init(&manager, 0, 1);
+
   return 0;
 }
